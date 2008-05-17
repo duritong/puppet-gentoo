84c9bfe50c4e818b29f0afaa9a5635f746783e05
mh <mh@immerda.ch>
2008-05-17 12:58:06 +0000

    codestyle

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,60 +5,52 @@
 # modules_dir { "gentoo": }
 
 class gentoo {
-    file { '/etc/portage/package.use':
-        owner => "root",
-        group => "0",
-        mode  => 644,
-        ensure => present,
+    file {'/etc/portage/package.use':
         source => [
             "puppet://$server/files/gentoo/package_use/$fqdn",
-            "puppet://$server/files/gentoo/package_use/default"
-        ]
+            "puppet://$server/files/gentoo/package_use/default",
+            "puppet://$server/gentoo/package_use/$fqdn",
+            "puppet://$server/gentoo/package_use/default"
+        ],
+        owner => root, group => 0, mode  => 0644;
     }
 
-    file { '/etc/portage/package.keywords':
-        owner => "root",
-        group => "0",
-        mode  => 644,
-        ensure => present,
+    file {'/etc/portage/package.keywords':
         source => [
             "puppet://$server/files/gentoo/package_keywords/$fqdn",
-            "puppet://$server/files/gentoo/package_keywords/default"
-        ]
+            "puppet://$server/files/gentoo/package_keywords/default",
+            "puppet://$server/gentoo/package_keywords/$fqdn",
+            "puppet://$server/gentoo/package_keywords/default"
+        ],
+        owner => root, group => 0, mode  => 0644;
     }
 
-    file { '/etc/portage/package.mask':
-        owner => "root",
-        group => "0",
-        mode  => 644,
-        ensure => present,
+    file {'/etc/portage/package.mask':
         source => [
             "puppet://$server/files/gentoo/package_mask/$fqdn",
-            "puppet://$server/files/gentoo/package_mask/default"
-        ]
+            "puppet://$server/files/gentoo/package_mask/default",
+            "puppet://$server/gentoo/package_mask/$fqdn",
+            "puppet://$server/gentoo/package_mask/default"
+        ],
+        owner => root, group => 0, mode  => 0644;
     }
 }
 
 define gentoo::deploymakeconf ($templatepath){
-    file {'host_makeconf':
-        path => '/etc/make.conf',
-        owner => root,
-        group => 0,
-        mode => 600,
+    file {'/etc/make.conf':
         content => template("$templatepath"),
+        owner => root, group => 0, mode => 0600;
    }
 }
 
 define gentoo::etcconfd (){
     file { "/etc/conf.d/${name}":
-        owner => "root",
-        group => "0",
-        mode  => 644,
         source => [
             "puppet://$server/files/gentoo/etc_conf.d/${name}_${fqdn}",
             "puppet://$server/files/gentoo/etc_conf.d/${name}_default",
             "puppet://$server/gentoo/etc_conf.d/${name}"
         ],
+        owner => root, group => 0, mode  => 0644;
     }
     if $require {
         File["/etc/conf.d/${name}"]{
